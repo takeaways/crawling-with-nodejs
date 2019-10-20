016c638ae7ca09c2033ef6664d20cf372735d02e
GeonilJang <jgi0105@naver.com>
2019-10-20 19:44:00 +0900

    xml csv axios cheerio

--- 001/index.js
+++ 001/index.js
@@ -25,31 +25,48 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const xlsx = require('xlsx');
 const workbook = xlsx.readFile('./001/data.xlsx');
+const add_to_sheet = require('./add_to_sheet');
 const ws = workbook.Sheets.영화목록;
 const records = xlsx.utils.sheet_to_json(ws);
 
 const crawler = async () => {
-  await Promise.all(records.map( async r =>{
+  add_to_sheet(ws, 'C1','s','point');
+  await Promise.all(records.map( async (r,i) =>{
     const response = await axios.get(r.링크);
     if(response.status === 200){
       const html = response.data;
       const $ = cheerio.load(html);
       const text = $('.score.score_left .star_score').text();
       console.log(r.제목, "평점 : ",text.trim());
+      const newCell = 'C'+(i+2);
+      add_to_sheet(ws, newCell,'n',parseFloat(text.trim()));
     }
   }));
+  xlsx.writeFile(workbook, './001/new.xlsx')
 }
 
-// crawler();
+crawler();
+//
+// (async function(){
+//   for(const [i ,r] of records.entries()){
+//     const response = await axios.get(r.링크);
+//     if(response.status === 200){
+//       const html = response.data;
+//       const $ = cheerio.load(html);
+//       const text = $('.score.score_left .star_score').text();
+//       console.log(r.제목, "평점 : ",text.trim());
+//     }
+//   }
+// })();
 
-(async function(){
-  for(const [i ,r] of records.entries()){
-    const response = await axios.get(r.링크);
-    if(response.status === 200){
-      const html = response.data;
-      const $ = cheerio.load(html);
-      const text = $('.score.score_left .star_score').text();
-      console.log(r.제목, "평점 : ",text.trim());
-    }
-  }
-})();
+
+// 004
+// const xlsx = require('xlsx');
+// const workbook = xlsx.readFile('./001/data.xlsx');
+// const ws = workbook.Sheets.영화목록;
+// const records = xlsx.utils.sheet_to_json(ws, {header:'A'});
+// ws['!ref'] = ws['!ref'].split(":").map((v,i)=>{
+//   if(i===0) return 'A2';
+//   return v;
+// }).join(':')
+// console.log(records);
--- README.md
+++ README.md
@@ -8,14 +8,14 @@
   - 데이터 베이스에 저장
   - 크롤링한 결과물을 다시 CSV, XLSX로 저장
 ## 장점
-2. 웹을 구성하는 언어가 자바스크립트기 때문에 호완성이 좋다.
+1. 웹을 구성하는 언어가 자바스크립트기 때문에 호완성이 좋다.
   - 생산성이 좋다.
 
 ### csv-parse 패키지로 csv 파싱하기
-1. npm init
-2. csv: 콤마와 줄 바꿈의로 구별된 값들
+3. npm init
+3. csv: 콤마와 줄 바꿈의로 구별된 값들
 3. npm i csv-parse 바퀴를 재발명하지 말아라
-4. const parse = require('csv-parse/lib/sync')
+3. const parse = require('csv-parse/lib/sync')
   - node_modules/csv-parse/lib/sync 함수를 require 하는 것으로 보면됩니다.
 
 ### xlsx패키지로  엑셀 파싱하기
